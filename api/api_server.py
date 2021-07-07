55ea067c85af8a673b04ef3d7c9a4d5605affe22
MaciejWitkowskiDev <321768@uwr.edu.pl>
2021-07-07 17:01:30 +0200

    added basic root request

--- api/api_server.py
+++ api/api_server.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    raise HTTPException(status_code=401, detail="Requests for root are not supported!")
