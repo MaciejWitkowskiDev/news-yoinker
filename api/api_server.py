e18379938c0ffcdc261174f47f858a07345956d7
MaciejWitkowskiDev <321768@uwr.edu.pl>
2021-07-07 17:38:38 +0200

    added iso languages list check

--- api/api_server.py
+++ api/api_server.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI, HTTPException
 from datetime import date
+from GoogleNews import GoogleNews
+from pycountry import languages
 
 app = FastAPI()
+
+def get_language_codes():
+    return [lang.iso639_1_code for lang in languages if hasattr(lang, 'iso639_1_code')]
+
+def check_valid_date():
+    pass
+
 
 @app.get("/")
 async def root():
@@ -12,4 +21,7 @@ async def read_item(phrases : list = [],
                     lang: str = "en",
                     start: str = '01/01/2000',
                     end : str = date.today().strftime("%d/%m/%Y")):
-    return 0
+    
+    gn_object = GoogleNews()
+    if lang not in get_language_codes():
+        raise HTTPException(status_code=410, detail=f"Lang {lang} is invalid. Please provide a correct ISO 639-1 language code.")
