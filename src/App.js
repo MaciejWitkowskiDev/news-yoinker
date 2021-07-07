c2f0445852246b79c6bb7be3380e8af3a75c27c0
MaciejWitkowskiDev <321768@uwr.edu.pl>
2021-07-07 22:41:00 +0200

    Added bootstrap basic template

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import Nav from './Nav';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
 const App = () => {
   return (
     <>
-      <h1>Hello world</h1>
+      <Nav />
+      <Container fluid>
+        <Row>
+          <Col>1 of 2</Col>
+          <Col>2 of 2</Col>
+        </Row>
+      </Container>
     </>
   );
 }
---
+++ src/Nav.js
@@ -0,0 +1,13 @@
+import Navbar from "react-bootstrap/Navbar";
+
+const Nav = () =>{
+    return(
+        <>
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="#home">News Yoinker</Navbar.Brand>
+            </Navbar>
+        </>
+    );
+}
+
+export default Nav
