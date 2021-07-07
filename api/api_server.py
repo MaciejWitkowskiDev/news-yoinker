f6a6a462389a5e09310f33a3333a55a3e559129d
MaciejWitkowskiDev <321768@uwr.edu.pl>
2021-07-07 17:23:20 +0200

    added the articles path, updated requirements

--- api/api_server.py
+++ api/api_server.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI, HTTPException
+from datetime import date
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     raise HTTPException(status_code=401, detail="Requests for root are not supported!")
+
+@app.get("/articles/")
+async def read_item(phrases : list = [],
+                    lang: str = "en",
+                    start: str = '01/01/2000',
+                    end : str = date.today().strftime("%d/%m/%Y")):
+    return 0
